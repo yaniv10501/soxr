2c1d40f2ecd83309fb5127a123970a201e5b4632
Yaniv Schweitzer <yaniv10501@gmail.com>
2026-03-05 20:53:02 +0200

    Add windows dllexport to CPP bridge;

--- src/soxr-bridge.cpp
+++ src/soxr-bridge.cpp
@@ -1,22 +1,28 @@
 #include "soxr.h"
 #include <vector>
 
+#if _MSC_VER // If we are compiling with the Microsoft compiler
+    #define EXPORT_API __declspec(dllexport)
+#else // For macOS/Linux (Clang/GCC)
+    #define EXPORT_API
+#endif
+
 // Define a simple structure to hold the resampler state
 extern "C" {
     // Factory function to create a resampler instance
-    void* CreateSoxr(double inRate, double outRate, int channels) {
+    EXPORT_API void* CreateSoxr(double inRate, double outRate, int channels) {
         soxr_error_t error;
-        // Using 'soxr_create' from the library 
+        // Using 'soxr_create' from the library
         soxr_t s = soxr_create(inRate, outRate, channels, &error, NULL, NULL, NULL);
         return (void*)s;
     }
 
     // The main processing function
-    int ProcessSoxr(void* resampler, float* input, int inLen, float* output, int outLen) {
+    EXPORT_API int ProcessSoxr(void* resampler, float* input, int inLen, float* output, int outLen) {
         soxr_t s = (soxr_t)resampler;
         size_t idone, odone;
 
-        // Process the buffers 
+        // Process the buffers
         // input: pointer to Unity's raw float array
         // output: pointer to the destination buffer
         soxr_error_t error = soxr_process(s, input, (size_t)inLen, &idone, output, (size_t)outLen, &odone);
@@ -24,11 +30,11 @@ extern "C" {
         return (int)odone; // Return how many samples were actually generated
     }
 
-    void DestroySoxr(void* resampler) {
+    EXPORT_API void DestroySoxr(void* resampler) {
         soxr_delete((soxr_t)resampler);
     }
 
-    int OneshotSoxr(
+    EXPORT_API int OneshotSoxr(
         double inRate,
         double outRate,
         float* input,
